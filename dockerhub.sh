4e03e0a0c752da95513b5aa2a113d33ccc7a77db
jose luis <josloe@gmail.com>
2021-06-25 00:30:28 +0200

    limpieza sh para generar los entornos

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,7 +1,3 @@
 
-cp dockerhub-compose.yaml lastcompose.yaml && \
-
-sed -i s#DOCKER_TAG#2021_06_24T21_55_46#g lastcompose.yaml && \
-nerdctl compose -f lastcompose.yaml down  && \
-nerdctl compose -f lastcompose.yaml up && \
-sed -i s#2021_06_24T21_55_46#DOCKER_TAG#g lastcompose.yaml
+nerdctl compose -f dockerhub.yaml down  && \
+nerdctl compose -f dockerhub.yaml up
