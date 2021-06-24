5a9bf64567a682954f1526846e48beb095f08f96
jose luis <josloe@gmail.com>
2021-06-25 00:19:59 +0200

    añadido latest como tag

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,5 +1,7 @@
 
-sed -i s#DOCKER_TAG#2021_06_24T11_01_24#g dockerhub-compose.yaml && \
-nerdctl compose -f dockerhub-compose.yaml down  && \
-nerdctl compose -f dockerhub-compose.yaml up && \
-sed -i s#2021_06_24T11_01_24#DOCKER_TAG#g dockerhub-compose.yaml
+cp dockerhub-compose.yaml lastcompose.yaml && \
+
+sed -i s#DOCKER_TAG#2021_06_24T21_55_46#g lastcompose.yaml && \
+nerdctl compose -f lastcompose.yaml down  && \
+nerdctl compose -f lastcompose.yaml up && \
+sed -i s#2021_06_24T21_55_46#DOCKER_TAG#g lastcompose.yaml
