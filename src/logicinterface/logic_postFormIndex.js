511c39c34f355fe204e1e4df268cde8b2d0979fd
jose luis <2064537+sosan@users.noreply.github.com>
2022-02-04 12:22:37 +0100

    /d desplegue

--- src/logicinterface/logic_postFormIndex.js
+++ src/logicinterface/logic_postFormIndex.js
@@ -371,16 +371,22 @@ const GetCarsByReservado = async (formulario) => {
 exports.CalcularTemporada = async (textoFechaRecogida) =>
 {
 
+    if (textoFechaRecogida === undefined) return;
     let isValidDate = Date.parse(textoFechaRecogida);
 
     if (isNaN(isValidDate) === true)
     {
-        const fechaSplitted = textoFechaRecogida.split("-");
-        const dia = fechaSplitted[0] - 0;
-        const mes = fechaSplitted[1] - 1;
-        const anyo = fechaSplitted[2] - 0;
-    
-        textoFechaRecogida = new Date(anyo, mes, dia   );
+        console.log("textoFechaRecogida" + textoFechaRecogida);
+        if (textoFechaRecogida.indexOf("-") != 0)
+        {
+            const fechaSplitted = textoFechaRecogida.split("-");
+            const dia = fechaSplitted[0] - 0;
+            const mes = fechaSplitted[1] - 1;
+            const anyo = fechaSplitted[2] - 0;
+        
+            textoFechaRecogida = new Date(anyo, mes, dia   );
+
+        }
     }
     
     const fechaRecogida = new Date(textoFechaRecogida);
